43de79738eaba79ea77fa4c8526822356ec4c4d3
Kaplun Ivan <109793717+LetMeIK@users.noreply.github.com>
2022-08-16 15:44:28 +0300

    Update SQL_2.sql

--- SQL_2.sql
+++ SQL_2.sql
@@ -3,17 +3,17 @@ SELECT
   name 
 FROM 
   Пилоты 
-  INNER JOIN (
+  INNER JOIN (  /*Соединяю выборки Пилотов и Рейсов с пассажирскими самолетами и кол-вом пассажиров > 30*/
     SELECT 
-      first_pilot_id, 
+      first_pilot_id,   /*Нахожу id подходящих пилотов (без возраста)*/
       second_pilot_id 
     FROM 
       Рейсы 
       INNER JOIN Самолеты ON Рейсы.plane_id = Самолеты.plane_id 
-      AND cargo_flg = 0 
+      AND cargo_flg = 0                           
       AND quantity > 30
   ) q_in 
-  ON pilot_id = q_in.first_pilot_id 
+  ON pilot_id = q_in.first_pilot_id   /*Пилот может быть как первым, так и вторым*/
   OR pilot_id = q_in.second_pilot_id 
 WHERE 
-  age > 45
+  age > 45   /*Учитываю условие на возраст*/
